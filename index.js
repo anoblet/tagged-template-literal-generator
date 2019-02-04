26fad0e3d151b295dc92ba40b2a371aae8503255
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-04 10:50:54 -0500

    Fix typescript in js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ export const generator = (tag, scope, props, template) => {
 }
 
 // Using a regular function instead of an arrow function allow us to bind (this) and retain context
-export const strToTag = function(str: string, tag: any) {
+export const strToTag = function(str, tag) {
   const convert = new Function("tag", "return tag`" + str + "`").bind(this);
   return convert(tag);
 };
